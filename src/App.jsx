80a65cd57a6e16fdc6e6b1fa26de3ac8f9d69f2d
NeenduDev <neendu.wickremasinghe@gmail.com>
2025-03-09 20:08:35 +0530

    Add Nested Routes and Index Routes

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={"/"} element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path={"product"} element={<Product />} />
         <Route path={"pricing"} element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path={"app"} element={<AppLayout />} />
+        <Route path={"app"} element={<AppLayout />}>
+          <Route index element={<p>List of sex</p>} />
+          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route path="countries" element={<p>Country</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path={"*"} element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
@@ -23,3 +28,7 @@ function App() {
 }
 
 export default App;
+/*
+-Wildcard (*) catches any route that is not explicitly defined. It's a fallback mechanism for undefined or unmatched paths.
+-Index is used inside a route to render a default component for a specific parent route, especially when that route is visited without navigating to any nested child route.
+*/
