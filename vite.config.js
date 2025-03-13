c2a2f5454f098f7a3d48df00866348d6e9a3e848
NeenduDev <neendu.wickremasinghe@gmail.com>
2025-03-13 22:22:27 +0530

    Implement programmatic navigation with useNavigate in onclick and Navigate component for index route

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslint({
+      failOnWarning: false, // Add this line
+      failOnError: false, // Add this line if you want to prevent stopping on errors as well
+    }),
+  ],
+  server: {
+    hmr: {
+      overlay: true,
+    },
+  },
 });
